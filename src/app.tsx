da33c3b432cf9080828bf8a3e75f5557ffe59527
Ramon de Oliveira Martins <raomartins@uneb.br>
2024-09-10 20:05:25 -0300

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import { Plus } from 'lucide-react';
+
+import letsStart from './assets/lets-start-illustration.svg';
+import logo from './assets/logo-in-orbit.svg';
+export function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="lets start illustration" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  );
+}
