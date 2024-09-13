c11725b87c9cd9b15a84528a36c0929f98efaa0c
Ramon de Oliveira Martins <raomartins@uneb.br>
2024-09-13 19:18:57 -0300

    feat: Using react query to fetch data and static text changed by dynamic values

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+import { App } from './app';
 
-import "./index.css";
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>
+  </QueryClientProvider>
+  // </StrictMode>
 );
